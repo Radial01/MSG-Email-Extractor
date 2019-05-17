9b617192400c04274a38b12aec84bd8126472bd1
Radial01 <50110449+Radial01@users.noreply.github.com>
2019-05-17 10:45:10 -0400

    Create emailExtractor.ps1

---
+++ emailExtractor.ps1
@@ -0,0 +1,21 @@
+# Modify email pattern to match the email address for which you are searching.
+$emailPattern = "([a-zA-Z0-9_\-\.]+)@gmail.com"
+$msgDirectory = "C:\msgFiles"
+
+# Parse .msg files, extracting email addresses that match the regex specified above.
+# Matches are placed in temp.txt.
+$Outlook = New-Object -ComObject Outlook.Application
+Get-ChildItem $msgDirectory -Filter *.msg |
+    ForEach-Object {
+        $msg = $outlook.Session.OpenSharedItem($_.FullName)
+        $msg.body
+        $results = ($msg.body | Select-String $pattern -AllMatches).Matches
+        foreach ($item in ($results)) { 
+            Add-Content -Path .\temp.txt $item.Value
+        }
+    }
+
+# Remove duplicate Entries from temp.txt and then remove temp.txt itself.
+$hash = @{}
+Get-Content -Path .\temp.txt | %{if($hash.$_ -eq $null) { $_ }; $hash.$_ = 1} > "List.txt"
+Remove-Item -Path .\temp.txt -Force
